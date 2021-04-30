948e2800bc3aa265487793e3e75cb7ac530cf30c
Jeremy Greer <jex.grizzle@gmail.com>
2021-04-30 07:11:28 -0400

    Finish LineTerminator

--- src/tokenizer/line-terminator.test.ts
+++ src/tokenizer/line-terminator.test.ts
@@ -18,4 +18,38 @@ describe('LineTerminator', () => {
       expect(actual).toEqual(expected)
     })
   })
+
+  describe('Carriage Return (U+000D) [lookahead != New Line (U+000A)]', () => {
+    it('should return the LineTerminator', () => {
+      const remainingInput = 'This stuff is not whiteSpace!'
+      const input = `\u000D${remainingInput}`
+      const actual = lineTerminator.getToken(input)
+      const expected: GetTokenResult = {
+        token: {
+          ignored: true,
+          type: 'LineTerminator',
+          value: '\u000D',
+        },
+        remainingInput,
+      }
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe('Carriage Return (U+000D) New Line (U+000A)', () => {
+    it('should return the LineTerminator', () => {
+      const remainingInput = 'This stuff is not whiteSpace!'
+      const input = `\u000D\u000A${remainingInput}`
+      const actual = lineTerminator.getToken(input)
+      const expected: GetTokenResult = {
+        token: {
+          ignored: true,
+          type: 'LineTerminator',
+          value: '\u000D\u000A',
+        },
+        remainingInput,
+      }
+      expect(actual).toEqual(expected)
+    })
+  })
 })
--- src/tokenizer/line-terminator.ts
+++ src/tokenizer/line-terminator.ts
@@ -1,8 +1,8 @@
 /*
 LineTerminator ::
   New Line (U+000A)
-  Carriage Return (U+000D)New Line (U+000A)
-  Carriage Return (U+000D)New Line (U+000A)
+  Carriage Return (U+000D) [lookahead != New Line (U+000A)]
+  Carriage Return (U+000D) New Line (U+000A)
 */
 
 import {GetToken} from './types'
@@ -27,8 +27,48 @@ const one: GetToken = (input) => {
   }
 }
 
+const two: GetToken = (input) => {
+  const char = input[0]
+  if (char === '\u000D' && input[1] !== '\u000A') {
+    const remainingInput = input.substring(1)
+    return {
+      token: {
+        ignored: true,
+        type: 'LineTerminator',
+        value: char,
+      },
+      remainingInput,
+    }
+  }
+  return {
+    token: null,
+    remainingInput: input,
+  }
+}
+
+const three: GetToken = (input) => {
+  const head = input.substring(0, 2)
+  if (head === '\u000D\u000A') {
+    const remainingInput = input.substring(2)
+    return {
+      token: {
+        ignored: true,
+        type: 'LineTerminator',
+        value: head,
+      },
+      remainingInput,
+    }
+  }
+  return {
+    token: null,
+    remainingInput: input,
+  }
+}
+
 export const getToken: GetToken = (input) => {
   return getFirstTokenMatch([
     one,
+    two,
+    three,
   ])(input)
 }
