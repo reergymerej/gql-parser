9d8a9d2f2658a43fe21931037c7727a7e0f18eb4
Jeremy Greer <jex.grizzle@gmail.com>
2021-05-01 10:05:37 -0400

    Finish IntegerPart

--- src/tokenizer/token/int-value/index.ts
+++ src/tokenizer/token/int-value/index.ts
@@ -1,4 +1,5 @@
 import {GetToken} from '../../types'
+import {findWhile} from '../../util'
 
 /*
 IntValue ::
@@ -42,6 +43,48 @@ export const isDigit = (char: string): boolean => {
 
 export const isNonZeroDigit = (char: string): boolean => {
   return char !== '0' && isDigit(char)
+}
+
+const findWhileNegativeSign = findWhile(isNegativeSign)
+
+const findWhileIsDigit = findWhile(isDigit)
+
+const getNegativeSign = (input: string): string => {
+  return findWhileNegativeSign(input).result
+}
+
+export const getIntegerPart: GetToken = (input) => {
+  const negativeSign = getNegativeSign(input)
+  const next = input[negativeSign.length]
+  const isZero = next === '0'
+  let value = `${negativeSign}${next}`
+  let remainingInput = input.substring(value.length)
+  if (isDigit(next)) {
+    if (isZero) {
+      return {
+        token: {
+            type: 'IntegerPart',
+            value,
+        },
+        remainingInput,
+      }
+    } else {
+      const digitsResult = findWhileIsDigit(remainingInput)
+      value = `${value}${digitsResult.result}`
+      remainingInput = remainingInput.substring(digitsResult.index)
+      return {
+        token: {
+            type: 'IntegerPart',
+            value,
+        },
+        remainingInput,
+      }
+    }
+  }
+  return {
+    token: null,
+    remainingInput: input,
+  }
 }
 
 const getToken: GetToken = function IntValue(input) {
