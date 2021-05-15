d2578a26925647e38a1a312c6afe0e375799eeaa
Jeremy Greer <jex.grizzle@gmail.com>
2021-05-14 20:40:47 -0400

    Find comment

--- src/tokenizer/ignored/comment.test.ts
+++ src/tokenizer/ignored/comment.test.ts
@@ -1,10 +1,11 @@
+import {crawler, CrawlerResult} from '../crawler'
 import {GetTokenResult} from '../types'
-import comment from './comment'
+import comment, { evaluate, Comment } from './comment'
 
 describe('Comment', () => {
   describe('negative test', () => {
     it('should NOT return the Comment', () => {
-      const input = `This is not a comment.`
+      const input = 'This is not a comment.'
       const actual = comment(input)
       const expected: GetTokenResult = {
         token: null,
@@ -16,7 +17,7 @@ describe('Comment', () => {
 
   describe('with empty CommentChar', () => {
     it('should return the Comment', () => {
-      const input = `#`
+      const input = '#'
       const actual = comment(input)
       const expected: GetTokenResult = {
         token: {
@@ -32,16 +33,48 @@ describe('Comment', () => {
 
   describe('with CommentChar', () => {
     it('should return the Comment', () => {
-      const input = `#I'm a little teapot.\u000AHere's my spout.`
+      const input = '#I\'m a little teapot.\u000AHere\'s my spout.'
       const actual = comment(input)
       const expected: GetTokenResult = {
         token: {
           type: 'Comment',
           ignored: true,
-          value: `#I'm a little teapot.`,
+          value: '#I\'m a little teapot.',
         },
-        remainingInput: `\u000AHere's my spout.`,
+        remainingInput: '\u000AHere\'s my spout.',
       }
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  fdescribe('Evaluator', () => {
+    it.each<[string, null | string]>([
+      [
+        '',
+        null,
+      ],
+      [
+        '#',
+        '#',
+      ],
+      [
+        '# Look at this!',
+        '# Look at this!',
+      ],
+    ])('should find %s', (prefix, expectedValue) => {
+      const remainingInput = '\u0019<- That is NOT a source char!'
+      const input = `${prefix === null ? '' : prefix}${remainingInput}`
+      const actual = crawler(input, evaluate)
+      const expectedResultValue = (expectedValue === null)
+        ? null
+        : {
+          type: 'Comment',
+          value: expectedValue as Comment['value'],
+        } as Comment
+      const expected: CrawlerResult<Comment> = [
+        expectedResultValue,
+        remainingInput,
+      ]
       expect(actual).toEqual(expected)
     })
   })
--- src/tokenizer/ignored/comment.ts
+++ src/tokenizer/ignored/comment.ts
@@ -1,48 +1,67 @@
-import {Count} from '../../types'
 import {isSourceChar} from '../source-character'
 import {GetToken} from '../types'
-import {assembler, findWhileByCharacter, isToken, Predicate, Requirement} from '../util'
-import lineTerminator from './line-terminator'
+import {crawler, EvaluationResult, Evaluator} from '../crawler'
 
 /*
 Comment ::
   # CommentChar (list, opt)
 
 CommentChar ::
-  SourceCharacter but not LineTerminator
+  SourceCharacter but not Comment
 */
 
-const isLineTerminator = isToken(lineTerminator)
-
-const isCommentChar: Predicate = input => {
-  return isSourceChar(input) && !isLineTerminator(input)
+export type SourceCharacter = {
+  type: 'SourceCharacter',
+  value: string
 }
-const isPound: Predicate = input => {
-  return input === '#'
-}
-const findWhileCommentChar = findWhileByCharacter(isCommentChar)
-const findWhileIsPound = findWhileByCharacter(isPound)
 
-const getToken: GetToken = (input) => {
-  const requirements: Requirement[] = [
-    {
-      count: Count.ONE,
-      finder: findWhileIsPound,
-    },
-    {
-      count: Count.ANY,
-      finder: findWhileCommentChar,
-    },
-  ]
-  const assembled = assembler(
-    requirements,
-    input,
-    'Comment',
-  )
-  if (assembled.token) {
-    assembled.token.ignored = true
+export type Comment = {
+  type: 'Comment',
+  value: string
+}
+
+const sourceChar: Evaluator<SourceCharacter> = (reader) => {
+  let read = ''
+  let allLength = reader.all().length
+  let good = ''
+  while (allLength--) {
+    read = reader.read(read.length + 1)
+    const lastRead = read[read.length - 1]
+    const lastReadIsSourceChar = isSourceChar(lastRead)
+    if (lastReadIsSourceChar) {
+      good += lastRead
+    } else {
+      break
+    }
   }
-  return assembled
+  const isFound = good !== ''
+  if (isFound) {
+    const value = good as SourceCharacter['value']
+    reader.consume(value.length)
+    const found: SourceCharacter = {
+      type: 'SourceCharacter',
+      value,
+    }
+    return found
+  }
+  return null
 }
 
-export default getToken
+export const evaluate: Evaluator<Comment> = (reader) => {
+  const head = reader.read(1)
+  const isFound = head === '#'
+  if (isFound) {
+    let value = head as Comment['value']
+    reader.consume(head.length)
+    const tail = sourceChar(reader)
+    const tailValue = tail === null ? '' : tail.value
+    value = `${head}${tailValue}` as Comment['value']
+    const found: Comment = {
+      type: 'Comment',
+      value,
+    }
+    return found
+  }
+  return null
+}
+
